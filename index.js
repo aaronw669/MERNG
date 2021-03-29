df38eddaa83e850141b97d927c99d9618e54b349
aaronw669 <78755850+aaronw669@users.noreply.github.com>
2021-03-29 19:35:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const Post = require("./models/Post");
+const { MONGODB } = require("./config.js");
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  }).then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   rootElement
+// );
